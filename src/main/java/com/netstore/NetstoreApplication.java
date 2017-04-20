604dd750da960132d5843b6cf6ae05dfb50ec179
MateuszSochacki <msochacki5@gmail.com>
2017-04-20 19:14:38 +0200

    initial commit

---
+++ src/main/java/com/netstore/NetstoreApplication.java
@@ -0,0 +1,12 @@
+package com.netstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetstoreApplication.class, args);
+	}
+}
